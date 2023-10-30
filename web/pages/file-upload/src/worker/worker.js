d939b70f300f8517990c4348a9c98771bb1d03d7
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-10-29 22:35:35 -0300

    feat(web): configure worker to use video processor class

--- web/pages/file-upload/src/worker/worker.js
+++ web/pages/file-upload/src/worker/worker.js
@@ -1,5 +1,41 @@
-onmessage = async () => {
-  setTimeout(() => {
-    self.postMessage({ status: 'done' })
-  }, 5000)
+import VideoProcessor from './videoProcessor.js'
+import MP4Demuxer from './mp4Demuxer.js'
+import CanvasRenderer from './canvasRenderer.js'
+
+const qvgaConstraints = { width: 320, height: 240 }
+// @todo: we could use this constraints in the future
+// const vgaConstraints = { width: 640, height: 480 }
+// const hdConstraints = { width: 1280, height: 720 }
+
+const encoderConfig = {
+  ...qvgaConstraints,
+  bitrate: 10e6,
+  //WebM
+  codec: 'vp09.00.10.08',
+  pt: 4,
+  hardwareAcceleration: 'prefer-software',
+
+  //MP4
+  // codec: 'avc1,42002A',
+  // pt: 1,
+  // hardwareAcceleration: 'prefer-hardware',
+  // avc: { format: 'annexb' },
+}
+
+const mp4Demuxer = new MP4Demuxer()
+const videoProcessor = new VideoProcessor({ mp4Demuxer })
+
+onmessage = async ({ data }) => {
+  const renderFrame = CanvasRenderer.getRenderer(data.canvas)
+
+  await videoProcessor.process({
+    file: data.file,
+    renderFrame,
+    encoderConfig,
+    sendMessage(message) {
+      self.postMessage(message)
+    },
+  })
+
+  self.postMessage({ status: 'done' })
 }
